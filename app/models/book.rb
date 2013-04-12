2c2a739cec53bee3420342545103aaf0d21fac75
Mason Chang <changmason@gmail.com>
2013-04-13 03:29:54 +0800

    add model validations for book

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,6 @@ class Book < ActiveRecord::Base
   attr_accessible :category_id, :author, :description, :publish_date, :title
 
   belongs_to :category
+
+  validates_presence_of :category_id, :author, :title
 end
